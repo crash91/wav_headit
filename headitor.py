e64037e5c73532daa4d6e11793ba42a90fa426f4
Archit Tamarapu <archit.tamarapu@gmail.com>
2020-06-30 16:19:23 +0200

    added class for header

--- headitor.py
+++ headitor.py
@@ -1,9 +1,71 @@
 from tkinter import *
 from tkinter import filedialog
-
+# from dataclass import dataclass
 import os
 import struct
 
+# @dataclass
+class wav_header:
+    riff: str = 'RIFF'
+    file_size: int
+    wave: str = 'WAVE'
+    fmt_chunk: str = 'fmt '
+
+    fmt_chunk_size: int = 16
+
+    audioformat: int = 1
+    num_channels: int
+    sample_rate: int
+    byte_rate: int
+    block_align: int
+    bits_per_sample: int
+
+    data_chunk: str = 'data'
+
+    data_size: int
+
+    def unpack_header(self, header_data):
+        self.riff = struct.unpack('>cccc', header_data[:4]) # 'RIFF'
+        self.file_size = struct.unpack('<I', header_data[4:8]) 
+        self.wave = struct.unpack('>cccc', header_data[8:12]) # 'WAVE'
+        self.fmt_chunk = struct.unpack('>cccc', header_data[12:16]) # 'fmt '
+
+        self.fmt_chunk_size = struct.unpack('<I', header_data[16:20]) # should always be 16
+
+        self.audioformat = struct.unpack('<H', header_data[20:22]) # should be 1 for PCM
+        self.num_channels = struct.unpack('<H', header_data[22:24]) 
+        self.sample_rate = struct.unpack('<I', header_data[24:28])
+        self.byte_rate = struct.unpack('<I', header_data[28:32]) # sample_rate * num_channels * bits_per_sample / 8
+        self.block_align = struct.unpack('<H', header_data[32:34]) # num_channels * bits_per_sample / 8
+        self.bits_per_sample = struct.unpack('<H', header_data[34:36])
+
+        self.data_chunk = struct.unpack('>cccc', header_data[36:40]) # 'data'
+
+        self.data_size = struct.unpack('<I', header_data[40:44]) # num_samples * num_channels * bits_per_sample / 8
+
+    def pack_header(self, buffer):
+        pass
+
+
+def update_fields():
+    for i in range(4):
+        textboxes[i].insert(0, header.riff[i])
+    
+    # for i in range(4, 8):
+        # textboxes[i].insert(0, str(header.file_size))
+
+    for i in range(8,12):
+        textboxes[i].insert(0, header.wave[i-8])
+
+    for i in range(12,16):
+        textboxes[i].insert(0, header.fmt_chunk[i-12])
+
+    # for i in range(16,20):
+        #textboxes[i].insert(0, header.fmt_chunk_size)
+
+    
+
+''' Button methods '''
 # populate fields when opening file
 def open_btn_click():
     wavfile = filedialog.askopenfilename(title = "Select WAV file",filetypes = (("Wave PCM","*.wav"),("All Files","*.*")))
@@ -12,27 +74,17 @@ def open_btn_click():
 
     # read the first 44 header bytes
     with open(wavfile, mode='rb') as wavfile:
-        header = wavfile.read(44)
+        header_data = wavfile.read(44)
 
-    riff = struct.unpack('>cccc', header[:4]) # 'RIFF'
-    file_size = struct.unpack('<I', header[4:8]) 
-    wave = struct.unpack('>cccc', header[8:12]) # 'WAVE'
-    fmt = struct.unpack('>cccc', header[12:16]) # 'fmt '
+    header.unpack_header(header_data)
+    update_fields()
 
-    fmt_chunk_size = struct.unpack('<I', header[16:20]) # should always be 16
+def write_btn_click():
+    pass
 
-    audioformat = struct.unpack('<H', header[20:22]) # should be 1 for PCM
-    num_channels = struct.unpack('<H', header[22:24]) 
-    sample_rate = struct.unpack('<I', header[24:28])
-    byte_rate = struct.unpack('<I', header[28:32]) # sample_rate * num_channels * bits_per_sample / 8
-    block_align = struct.unpack('<H', header[32:34]) # num_channels * bits_per_sample / 8
-    bits_per_sample = struct.unpack('<H', header[34:36])
 
-    data_chunk = struct.unpack('>cccc', header[36:40]) # 'data'
-
-    data_size = struct.unpack('<I', header[40:44]) # num_samples * num_channels * bits_per_sample / 8
-
-    header_label.configure(text=header)
+''' Main window config and loop '''
+header = wav_header()
 
 window = Tk()
 
@@ -40,13 +92,22 @@ window.title("WAV Headitor")
 window.geometry('600x400')
 
 label = Label(window, text="Please open a WAV file!")
-label.grid(column=0, row=1)
+label.grid(column=0, row=1, columnspan=8)
 
 open_btn = Button(window, text="Open", command=open_btn_click)
-open_btn.grid(column=0, row=0)
+open_btn.grid(column=0, row=0, columnspan=8)
 
-header_label = Label(window, text='Header placeholder')
-header_label.grid(column=0, row=2)
+for byte_num in range(44):
+    r = 2*(byte_num//8) + 2
+    c = byte_num%8
+    Label(window, text=str(byte_num)).grid(row=r, column=c)
+
+textboxes = []
+for byte_num in range(44):
+    r = 2*(byte_num//8) + 3
+    c = byte_num%8
+    textboxes.append(Entry(window, width=4))
+    textboxes[-1].grid(row=r, column=c)
 
 window.mainloop()
 
